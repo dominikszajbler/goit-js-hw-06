0cce99476454c29f9f9c7329815fa30ded5b8365
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-17 22:24:59 +0100

    task02 ended

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// console.log(document.querySelector('ul#ingredients'));
+const ingredientsUl = document.querySelector('ul#ingredients');
+
+// const liItem = document.createElement('li');
+// liItem.textContent = "owoc";
+// item.append(liItem)
+
+ingredients.forEach(ingredient => {
+  const liItem = document.createElement('li');
+  liItem.textContent = ingredient;
+  liItem.className = "item";
+  ingredientsUl.append(liItem)
+  }
+)
