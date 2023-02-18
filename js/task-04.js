39c6d8e7c3bd33fa34748ea8cdfb433d963e7fd8
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-18 19:18:19 +0100

    task05 started

--- js/task-04.js
+++ js/task-04.js
@@ -1,19 +1,24 @@
-
-let counterValue = 0
+let counterValue = 0;
 
 const displayCounter = document.querySelector("#value");
 
 const decrement = document.querySelector('button[data-action="decrement"]');
 
 const subtractClick = (event) => {
-	return (counterValue = counterValue - 1), displayCounter.textContent = counterValue;
+	return (
+		(counterValue = counterValue - 1),
+		(displayCounter.textContent = counterValue)
+	);
 };
 decrement.addEventListener("click", subtractClick);
 
 const increment = document.querySelector('button[data-action="increment"]');
 
 const addClick = (event) => {
-	return (counterValue = counterValue + 1), displayCounter.textContent = counterValue;
+	return (
+		(counterValue = counterValue + 1),
+		(displayCounter.textContent = counterValue)
+	);
 };
 increment.addEventListener("click", addClick);
 
