4f9e2d26cb1571f93915dccab183e7fd6dee8d30
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-18 08:57:39 +0100

    task03 ended

--- js/task-03.js
+++ js/task-03.js
@@ -12,16 +12,39 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
-console.log(document);
+// console.log(document);
 
 // const imagesUl = document.querySelector('ul#ingredients');
 const imagesUl = document.querySelector('ul');
-console.log(imagesUl);
+// console.log(imagesUl);
 
-images.forEach(img => {
-  const liItem = document.createElement('li');
-  liItem.textContent = img.alt;
-  // liItem.className = "item";
-  imagesUl.append(liItem)
-  }
-)
+const image = images.map(({url, alt}) => `<li class ="img"> <img src=${url} alt="${alt}" width="250"/></>`).join("");
+imagesUl.insertAdjacentHTML("beforeend", image);
+imagesUl.style.display = 'flex';
+imagesUl.style.justifyContent = 'space-between';
+
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+
+
+// console.log(document);
+// images.forEach(img => {
+//   const liItem = document.createElement('li');
+//   // liItem.textContent = img.alt;
+//   // liItem.className = "item";
+//   imagesUl.append(liItem)
+//   }
+// )
+// /* <img
+//   src="https://images.pexels.com/photos/67112/pexels-photo-67112.jpeg"
+//   alt="Macbook Air na szarym drewnianym stole"
+//   width="400"
+// /> *
+// const markup = newTechnologies
+//   .map((technology) => `<li class="list-item new">${technology}</li>`)
+//   .join("");
+
+// list.insertAdjacentHTML("beforeend", markup);
+// list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
