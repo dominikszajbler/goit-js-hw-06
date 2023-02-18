f6a2b85958f2b89f33e06d890abac4566c517f03
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-18 20:04:37 +0100

    task05 ended

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const inputValue = document.querySelector('input#name-input');
+
+const outputValue = document.querySelector('span#name-output');
+
+inputValue.addEventListener("input", (event) => {
+    outputValue.textContent = event.currentTarget.value;
+})
