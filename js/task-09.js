80fa7d43b36f4a3f4db90585d98a8827f6038ac2
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-20 22:57:06 +0100

    task09 ended

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", () => {
+  const color= getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  colorName.textContent = color;
+}
+)
