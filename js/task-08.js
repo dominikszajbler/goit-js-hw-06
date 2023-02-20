12785aa7342c0ae40d30e965e2c0bef0477fe064
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-20 22:34:04 +0100

    task08 ended

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,3 @@
-// const form = document.querySelector(".login-form");
-// console.log(form);
-
-
-
-
-
-
-
-
-
 const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", handleSubmit);
@@ -22,24 +11,9 @@ function handleSubmit(event) {
   if (email.value === "" || password.value === "") {
     return window.alert("Fill all the fields of gold!");
   }
-  console.log(`{Login: ${email.value}, Password: ${password.value}}`);
-//   event.currentTarget.reset();
+  const output = `{ email: "${email.value}", password: "${password.value}", }`;
+  console.log(output);
+  event.currentTarget.reset();
 }
 
 
-// const form = document.querySelector(".form");
-
-// form.addEventListener("submit", handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const {
-//     elements: { login, password }
-//   } = event.currentTarget;
-
-//   if (login.value === "" || password.value === "") {
-//     return console.log("Please fill in all the fields!");
-//   }
-
-//   console.log(`Login: ${login.value}, Password: ${password.value}`);
-//   event.currentTarget.reset();
