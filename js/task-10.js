d9e5eed1fa55a2a79020382e534e54f4d8b07577
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-02-22 20:23:59 +0100

    task10 ready

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const qs = (selector) => document.querySelector(selector);
+
+const boxes = qs('#boxes');
+const creator = qs('button[data-create]');
+const destoyer = qs('button[data-destroy]');
+const number = qs('#controls input');
+
+const createBoxes = (amount) => {
+  let boxesArr = [];
+  for (let i =1; i <=amount; i++){
+    const box = document.createElement('div');
+    box.style.height = `${(20 + i * 10)}px`;
+    box.style.width = `${(20 + i * 10)}px`;
+		box.style.borderColor = "black";
+		box.style.borderStyle = "solid";
+		box.style.borderWidth = "1px";
+		box.style.margin = "10px";
+		box.style.background = getRandomHexColor();
+		boxesArr = [...boxesArr, box];
+  }
+  boxes.append(...boxesArr);
+};
+
+creator.addEventListener('click', e => createBoxes(number.value));
+destoyer.addEventListener('click', e => boxes.innerHTML = '');
