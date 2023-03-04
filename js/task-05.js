572eb1df24932d6dae743cc9c72ebaa968f884ce
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-03-04 08:29:03 +0100

    romeing

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,9 @@
 const inputValue = document.querySelector("input#name-input");
 
 const outputValue = document.querySelector("span#name-output");
-const defaultText = outputValue.textContent;	
-inputValue.addEventListener(
-	"input",
-	(event) => (event.currentTarget.value === "" ? outputValue.textContent =  defaultText :
-		outputValue.textContent = event.currentTarget.value),
-	
+const defaultText = outputValue.textContent;
+inputValue.addEventListener("input", (event) =>
+	event.currentTarget.value === ""
+		? (outputValue.textContent = defaultText)
+		: (outputValue.textContent = event.currentTarget.value),
 );
